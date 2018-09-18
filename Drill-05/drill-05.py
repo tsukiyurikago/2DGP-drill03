109afb52c3caebe6a3f9508712bc68514f3aded0
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-09-18 18:40:12 +0900

    drill-05진짜실습2
    move dot to dot함수를 정의했다

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -1,10 +1,23 @@
 from pico2d import *
-
 open_canvas()
-
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
+
+def move_dot_to_dot(x1, y1, x2, y2)
+    pass
+
+while True:
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
+    move_dot_to_dot(203,535,132,243)
 
 
-    
 close_canvas()
+
