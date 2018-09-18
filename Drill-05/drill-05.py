ab3f869d0f0703a213f53505a78647989eef3ae6
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-09-18 19:48:20 +0900

    drill-05진짜 실습-6
    가독성이 낮은것 같아 함수 이름을 직관적으로 바꿨다move dot to dot -> run anim dot to dot

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -3,7 +3,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def move_dot_to_dot(x1, y1, x2, y2):
+def run_anim_dot_to_dot(x1, y1, x2, y2):
     animnum = 0
     if x2 > x1:
         animnum  = 1
@@ -25,16 +25,16 @@ def move_dot_to_dot(x1, y1, x2, y2):
         delay(0.05)
 
 while True:
-    move_dot_to_dot(203,535,132,243)
-    move_dot_to_dot(132,243,535,470)
-    move_dot_to_dot(535,470,477,203)
-    move_dot_to_dot(477,203,715,136)
-    move_dot_to_dot(715,136,316,225)
-    move_dot_to_dot(316,225,510,92)
-    move_dot_to_dot(510,92,692,518)
-    move_dot_to_dot(692,518,682,336)
-    move_dot_to_dot(682,336,712,349)
-    move_dot_to_dot(712,349,203,535)
+    run_anim_dot_to_dot(203,535,132,243)
+    run_anim_dot_to_dot(132,243,535,470)
+    run_anim_dot_to_dot(535,470,477,203)
+    run_anim_dot_to_dot(477,203,715,136)
+    run_anim_dot_to_dot(715,136,316,225)
+    run_anim_dot_to_dot(316,225,510,92)
+    run_anim_dot_to_dot(510,92,692,518)
+    run_anim_dot_to_dot(692,518,682,336)
+    run_anim_dot_to_dot(682,336,712,349)
+    run_anim_dot_to_dot(712,349,203,535)
 
 
 close_canvas()
