fe7dbf8ab497d87b5b5190dfec5a4a94eb08b378
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-09-20 18:02:39 +0900

    drill-06-2
    캐릭터 키로 움직이는 함수를 완성했다

---
+++ Drill-06/move_character_with_key.py
@@ -0,0 +1,44 @@
+from pico2d import *
+
+
+def handle_events():
+    global running
+    global x
+    global row
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x = x + 10
+                row = 0
+            elif event.key == SDLK_LEFT:
+                x = x - 10
+                row = 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x = 800 // 2
+frame = 0
+row = 0
+
+while running:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 100 * row, 100, 100, x, 90)
+    update_canvas()
+
+    handle_events()
+    frame = (frame + 1) % 8
+
+    delay(0.05)
+
+close_canvas()
+
