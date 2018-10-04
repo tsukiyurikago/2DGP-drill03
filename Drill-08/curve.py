d424c5873e1c7529c2768155b7ac64808a7f7a80
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-10-04 18:06:39 +0900

    drill-8-3
    시작점과끝점이달라지면 곡선이 달라지는것을 확인하였다

--- Drill-08/curve.py
+++ Drill-08/curve.py
@@ -103,7 +103,8 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 prepare_turtle_canvas()
 
-draw_curve_3_points((-270,-100),(0,50,),(50,120))
+draw_curve_3_points((-270,-100),(0,50),(50,120))
+draw_curve_3_points((-50,-150),(150,-100),(-350,-100))
 
 
 
