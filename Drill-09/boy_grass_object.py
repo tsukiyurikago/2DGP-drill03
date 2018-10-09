1410a514d5ac17f7b0a868eff20419a7cba5ad8a
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-10-09 17:31:06 +0900

    drill-9-3
    소년 클래스를 완성했다

--- Drill-09/boy_grass_object.py
+++ Drill-09/boy_grass_object.py
@@ -7,6 +7,17 @@ class Grass:
     def draw(self):
         self.image.draw(400,30)
 
+class Boy:
+    def _init_(self):
+        self.x,self.y=0,90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame+1)%8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+
 def handle_events():
     global running
     events = get_events()
