586beeec111034cfbd6be87e54d8ac8a0394ecc5
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-10-11 17:39:16 +0900

    drill-9-2
    타이틀상태와 시작상태까지 작성하고 실행됨을 확인했습니다

--- Drill-10/start_state.py
+++ Drill-10/start_state.py
@@ -1,4 +1,5 @@
 import game_framework
+import title_state
 from pico2d import *
 
 
@@ -8,19 +9,31 @@ logo_time = 0.0
 
 
 def enter():
-    pass
+    global image
+    image = load_image('kpu_credit.png')
 
 
 def exit():
-    pass
+    global image
+    del(image)
 
 
 def update():
-    pass
+    global logo_time
+
+    if(logo_time > 1.0):
+        logo_time = 0
+        #game_framework.quit()
+        game_framework.push_state(title_state)
+    delay(0.01)
+    logo_time += 0.01
 
 
 def draw():
-    pass
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 
