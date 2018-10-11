c21101293fc45d5e13006f8f2a8b8f9bfe68d44f
tsukiyurikago <43169695+tsukiyurikago@users.noreply.github.com>
2018-10-11 17:45:56 +0900

    drill-9-3
    메인상태를 작성하고 타이틀상태에 추가하여 구동하는것을 확인했습니다.

--- Drill-10/main_state.py
+++ Drill-10/main_state.py
@@ -46,10 +46,15 @@ class Boy:
 
 
 def enter():
-    pass
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
 
 
 def exit():
+    global boy, grass
+    del(boy)
+    del(grass)
     pass
 
 
@@ -62,15 +67,23 @@ def resume():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
 
 
 def update():
-    pass
+    boy.update()
 
 
 def draw():
-    pass
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
 
 
 
--- Drill-10/title_state.py
+++ Drill-10/title_state.py
@@ -1,4 +1,5 @@
 import game_framework
+import main_state
 from pico2d import *
 
 
@@ -24,6 +25,8 @@ def handle_events():
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
 
 
 def draw():
