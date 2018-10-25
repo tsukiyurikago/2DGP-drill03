098175728d269ad522819e8019d98be844831601
tsukiyurikago <kus2twi@gmail.com>
2018-10-25 18:45:05 +0900

    drill11-4
    compositdraw수정

--- Drill-11/boy.py
+++ Drill-11/boy.py
@@ -145,7 +145,7 @@ class SleepState:
         if boy.dir == 1:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, 3.14 / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
         else:
-            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, -3.14 / 2, ' ', boy.x - 25, boy.y - 25, 100, 100)
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, 3.14 / 2, ' ', boy.x + 25, boy.y - 25, 100, 100)
 
 
 
@@ -156,7 +156,7 @@ next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SLEEP_TIMER: SleepState, SPACE: IdleState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, SPACE: RunState, SHIFT_DOWN: DashState, SHIFT_UP: RunState},
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState, LEFT_UP: RunState, RIGHT_UP: RunState, SPACE: IdleState},
-    DashState: {RUN_TIMER: RunState, SHIFT_UP: RunState}
+    DashState: {RUN_TIMER: RunState, SHIFT_UP: RunState, RIGHT_UP: IdleState, LEFT_UP: IdleState}
 }
 
 class Boy:
